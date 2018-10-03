8470d251bfd288eb08a2e0ed09a495048eae6c69
root <root@pr1malbyt3s.c.my-project-1523574485856.internal>
2018-10-03 22:32:57 +0000

    Install script upload.

---
+++ PostgreSQL_Docker_Setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Set color variables.
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+#Ensure container not already running.
+echo -e "${GREEN}Ensuring PostgreSQL container not already running.${NC}"
+docker stop postgresql_playground > /dev/null 2>&1
+sleep 2
+docker rm postgresql_playground > /dev/null 2>&1
+
+#Initialize PostgreSQL container, link localhost port to docker port and give time for container to start.
+echo -e "${GREEN}Starting PostgreSQL container.${NC}"
+docker run --name postgresql_playground --restart=always -d -p 127.0.0.1:5432:5432 postgres
+sleep 3
+
+#Create password for postgres default user.
+#NEED TO ADD PASSWORD CHECK METHOD!
+echo -e "${GREEN}You will be prompted for new PostgreSQL password for user postgres.${NC}"
+docker exec -it postgresql_playground psql -U postgres -c "\password postgres"
+
+#Setup finished. Inform user.
+echo -e "${GREEN}PostgreSQL Docker container setup complete!${NC}"
+echo -e "${GREEN}You can access your database at localhost port 5342.${NC}"
